6e9ca4bdd55596a2e74ae37fb84778c725ad1db2
micahmarc <micahmarc16@gmail.com>
2024-09-17 00:27:25 -0500

    YouAreAwesome V.1

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,56 +12,48 @@ struct ContentView: View {
     
     var body: some View {
         
-      
-            VStack {
-                Text("You Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(
-                        Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .cornerRadius(15)
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                //.border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                Spacer()
-                Spacer()
-                
-                HStack{
-                    Button("Awesome") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        //This is the action when the button is pressed
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                        //This is the action when the button is pressed
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                //.border(.purple, width: 5)
+        VStack {
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            //.border(.orange, width: 1)
                 .padding()
+            
+            Spacer()
+            
+            
+            HStack{
+                Button("Awesome") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    //This is the action when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
                 
+                Spacer()
+                
+                Button("Great") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    //This is the action when the button is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            //.border(.purple, width: 5)
+            .padding()
+            
         }
     }
+}
+
 
 #Preview {
     ContentView()

4a3bdc10f21e42620e16c5b6b6118758615e2367
Micah Palmer <micahmarc16@gmail.com>
2024-09-20 22:57:12 -0500

    YouAreAwesome V.3

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    
+    
+    @State private var messageString = "Namaste"
     
     var body: some View {
-        
         VStack {
             
             Spacer()
+            
+            Image(systemName: "speaker.wave.2", variableValue: 0.51)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
+                .cornerRadius(30)
+                .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 1)
+                )
+                .padding()
             
             Text(messageString)
                 .font(.largeTitle)
@@ -24,7 +39,6 @@ struct ContentView: View {
                 .foregroundColor(.red)
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
-            //.border(.orange, width: 1)
                 .padding()
             
             Spacer()
@@ -47,7 +61,6 @@ struct ContentView: View {
                 }
                 .buttonStyle(.borderedProminent)
             }
-            //.border(.purple, width: 5)
             .padding()
             
         }

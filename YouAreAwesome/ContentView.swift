74320518576697f38c2bc71c68a5f0e5de85cdcc
Micah Palmer <micahmarc16@gmail.com>
2024-09-30 23:41:38 -0500

    YouAreAwesome V.4

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,27 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     
-    @State private var messageString = "Namaste"
+    @State private var messageString = " "
     
     var body: some View {
         VStack {
             
             Spacer()
-            
-            Image(systemName: "speaker.wave.2", variableValue: 0.51)
-                .resizable()
-                .scaledToFit()
-                .symbolRenderingMode(.multicolor)
-                .padding()
-                .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
-                .cornerRadius(30)
-                .shadow(color: .gray, radius: 30, x: 20, y: 20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(.teal, lineWidth: 1)
-                )
-                .padding()
-            
+                        
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -40,24 +26,20 @@ struct ContentView: View {
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
                 .padding()
-            
-            Spacer()
-            
-            
-            HStack{
-                Button("Awesome") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    //This is the action when the button is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
                 
                 Spacer()
                 
-                Button("Great") {
+                Button("Show Message") {
+                    let message1 = "You Are Awesome!"
+                    let message2 = "You Are Great!"
+                    
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                     //This is the action when the button is pressed
-                    messageString = "You Are Great!"
+                    if messageString == message1 {
+                        messageString = message2
+                    } else {
+                        messageString = message1
+                    }
                 }
                 .buttonStyle(.borderedProminent)
             }
@@ -65,7 +47,7 @@ struct ContentView: View {
             
         }
     }
-}
+
 
 
 #Preview {

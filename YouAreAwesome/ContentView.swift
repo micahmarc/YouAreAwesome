e697b9434801e88e2b9535444f1e1ebe9e0a5914
Micah Palmer <micahmarc16@gmail.com>
2024-10-16 21:54:03 -0500

    YouAreAwesome V.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     
     
-    @State private var messageString = " "
+    @State private var messageString = ""
+    @State private var imageName: String = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     
     var body: some View {
         VStack {
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
             
             Spacer()
                         
@@ -30,16 +40,25 @@ struct ContentView: View {
                 Spacer()
                 
                 Button("Show Message") {
-                    let message1 = "You Are Awesome!"
-                    let message2 = "You Are Great!"
-                    
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    //This is the action when the button is pressed
-                    if messageString == message1 {
-                        messageString = message2
-                    } else {
-                        messageString = message1
+                    let messages = ["You Are Awesome!",
+                                    "You Are Great",
+                                    "You Are Fantastic!",
+                                    "Fabulous?, That's You!",
+                                    "You Make Me Smile!",
+                                    "When the Genius Bar Needs Help, Thet Call You!"]
+                    messageString = messages[messageNumber]
+                    messageNumber += 1
+                    if messageNumber == messages.count {
+                        messageNumber = 0
                     }
+                    
+                    
+                    imageName = "image\(imageNumber)"
+                    imageNumber = imageNumber + 1
+                    if imageNumber > 9 {
+                        imageNumber = 0
+                    }
+                    print(imageNumber)
                 }
                 .buttonStyle(.borderedProminent)
             }
